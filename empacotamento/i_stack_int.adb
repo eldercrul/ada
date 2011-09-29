b5aae6e1e7986427f109865b847b8b5a5109dbab
aramisf <aramis.fernandes@gmail.com>
2011-09-29 11:31:15 -0300

    Corrigido lib int

--- empacotamento/i_stack_int.adb
+++ empacotamento/i_stack_int.adb
@@ -1,5 +1,5 @@
 package body I_Stack is
-   -- Push an integer.  If the stack is already full, ignore the push.
+
    procedure Push(S: in out Int_Stack; I: Integer) is
    begin
       if S.Size < Max_Size then
@@ -8,7 +8,7 @@ package body I_Stack is
       end if;
    end Push;
 
-   -- Pop and integer.  If the stack is empty, a zeor is placed in I.
+
    procedure Pop(S: in out Int_Stack; I: out Integer) is
    begin
       if S.Size > 0 then
@@ -17,7 +17,7 @@ package body I_Stack is
       end if;
    end Pop;
 
-   -- Get the top item, or 0 if the stack is empty.
+
    function Top(S: Int_Stack) return Integer is
    begin
       if S.Size > 0 then
@@ -27,19 +27,19 @@ package body I_Stack is
       end if;
    end Top;
 
-   -- Tell if the stack is empty.
+
    function Empty(S: Int_Stack) return Boolean is
    begin
       return S.Size = 0;
    end Empty;
 
-   -- Tell if the stack is empty.
+
    function Full(S: Int_Stack) return Boolean is
    begin
       return S.Size = Max_Size;
    end Full;
 
-   -- Make the stack empty.
+
    procedure Clean(S: in out Int_Stack) is
    begin
       S.Size := 0;
--- empacotamento/i_stack_int.ads
+++ empacotamento/i_stack_int.ads
@@ -2,10 +2,9 @@ package I_Stack is
 
    type Int_Stack is private;
 
-   -- Max stack size.
+
    Max_Size: constant Integer := 100;
 
-   -- All the public stack operations.
    procedure Push(S: in out Int_Stack; I: Integer);
    procedure Pop(S: in out Int_Stack; I: out Integer);
    function Top(S: Int_Stack) return Integer;
@@ -14,10 +13,11 @@ package I_Stack is
    procedure Clean(S: in out Int_Stack);
 
    private
-      -- The items here cannot be accessed by clients of the package.
+
       type Stack_Data_Type is array(1..Max_Size) of Integer;
       type Int_Stack is record
          Size: Integer := 0;
          Data: Stack_Data_Type;
       end record;
+
 end I_Stack;
